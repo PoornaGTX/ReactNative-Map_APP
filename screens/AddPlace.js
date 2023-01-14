50162413477c40771c1b0221d5673e7664308699
Poorna Ranasinghe <onlinepoorna@gmail.com>
2023-01-14 09:15:11 +0530

    init commit

---
+++ screens/AddPlace.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import PlaceForm from "../components/Places/PlaceForm";
+
+const AddPlace = () => {
+  return <PlaceForm />;
+};
+
+export default AddPlace;
---
+++ screens/AllPlaces.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import PlacesList from "../components/Places/PlacesList";
+
+const AllPlaces = () => {
+  return <PlacesList />;
+};
+
+export default AllPlaces;
